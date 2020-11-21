bf0dae6e5f3e9a6c57b72b7aedf8a3f2b7b83177
ridam258 <ridam258@gmail.com>
2020-11-21 11:37:50 +0530

    added newgame option and score counter

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+var counter1=0;
+var counter2=0;
+var newGameButton=document.querySelector("#newGame");
+newGameButton.addEventListener("click",pressingNewGame);
+function pressingNewGame() {
+   document.getElementById("noneDisplay").style.display="block";  
+   document.getElementById("topHeading").innerHTML="PRESS THE PLAY BUTTON TO PLAY!!";
+   document.getElementById("newGame").style.display="none";
+   counter1=0;
+   counter2=0;
+   document.querySelector("#player1Counter").innerHTML=counter1;
+    document.querySelector("#player2Counter").innerHTML=counter2;
+    document.querySelector("#player1head").innerHTML="PLAYER-1";
+    document.querySelector("#player2head").innerHTML="PLAYER-2";
+}
+
+
+
+
 var playButton=document.querySelector(".play");
 playButton.addEventListener("click",mainGame);
+
+
+
 
 function mainGame() {
     var image1=document.querySelector("#pic1");
@@ -10,19 +32,46 @@ function mainGame() {
     var randomNumber2=(Math.floor(Math.random()*3))+1;
 var randomImage2="pic"+randomNumber2+".png";
     image2.setAttribute("src",randomImage2);
+    
+
+
 
 
 
     if((randomNumber1===1&&randomNumber2===3)||(randomNumber1===2&&randomNumber2===1)||(randomNumber1===3&&randomNumber2===2)){
-        document.querySelector("h1").innerHTML="PLAYER-1 WINS";
+        document.querySelector("#player1head").innerHTML="PLAYER-1 WINS";
+        document.querySelector("#player2head").innerHTML="PLAYER-2";
+        counter1+=1;
+        document.querySelector("#player1Counter").innerHTML=counter1;
+        if(counter1===10){
+            document.getElementById("noneDisplay").style.display="none";  
+            document.getElementById("topHeading").innerHTML="PLAYER-1 WINS </br> PLAY AGAIN";
+            document.getElementById("newGame").style.display="block";
+            
+        }
     }
     else if((randomNumber1===3&&randomNumber2===1)||(randomNumber1===1&&randomNumber2===2)||(randomNumber1===2&&randomNumber2===3)){
-        document.querySelector("h1").innerHTML="PLAYER-2 WINS";
+        document.querySelector("#player2head").innerHTML="PLAYER-2 WINS";
+        document.querySelector("#player1head").innerHTML="PLAYER-1";
+        counter2+=1;
+    document.querySelector("#player2Counter").innerHTML=counter2;
+
+        if(counter2===10){
+            document.getElementById("noneDisplay").style.display="none";  
+            document.getElementById("topHeading").innerHTML="PLAYER-2 WINS </br> PLAY AGAIN";
+            document.getElementById("newGame").style.display="block";
+            
+        }
+
     }
     else{
-        document.querySelector("h1").innerHTML="DRAW! PLAY AGAIN";
+        document.querySelector("#player1head").innerHTML="DRAW! PLAY AGAIN";
+        document.querySelector("#player2head").innerHTML="DRAW! PLAY AGAIN";
+
     }
+    
 }
+
  
 
 
